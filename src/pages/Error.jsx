7b47cbba86794eda056ed5e883151cccaf21c0cd
Miftah <miftahaadem@gmail.com>
2024-11-11 17:55:23 +0300

    add ErrorPage component for handling 404 and other errors

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,30 @@
+import { Link, useRouteError } from 'react-router-dom'
+
 const Error = () => {
+  const error = useRouteError()
+  console.log(error)
+  if (error.status === 404) {
+    return (
+      <main className="min-h-[100vh] grid place-items-center px-8">
+        <div className="text-center">
+          <h1 className="text-9xl font-semibold text-primary">404</h1>
+          <h3 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl capitalize">
+            page not found
+          </h3>
+          <p className="mt-6 text-lg leading-7 ">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <Link to="/" className="btn btn-primary capitalize mt-10">
+            back to home
+          </Link>
+        </div>
+      </main>
+    )
+  }
   return (
-    <div>Error</div>
+    <main>
+      <h3>there was an error...</h3>
+    </main>
   )
 }
 export default Error
