cd36e0094d395c64e4c51ec70da5695805a65d37
Miftah <miftahaadem@gmail.com>
2024-11-11 16:52:08 +0300

    setup all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from './pages'
 const App = () => {
   return <div className="text-3xl underline"> App</div>
 }
